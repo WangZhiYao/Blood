49bc16d9e4286203207d7637aa587eb89386c111
WangZhiYao <393893729@qq.com>
2021-02-27 21:51:26 +0800

    feat: update version code & version name
    Signed-off-by: WangZhiYao <393893729@qq.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "me.zhiyao.blood"
         minSdkVersion(21)
         targetSdkVersion(30)
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 11
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
